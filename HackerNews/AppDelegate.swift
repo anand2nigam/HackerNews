8e62ff6715728fd517be41dd4170c56c543e9978
Anand Nigam <anandnigam.2aug@gmail.com>
2018-12-30 14:57:20 +0530

    LogIn Screen Created and Configured with Firebase.

--- HackerNews/AppDelegate.swift
+++ HackerNews/AppDelegate.swift
@@ -7,17 +7,34 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
+
+    
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        
+        
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url, sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String, annotation: [ : ])
+    }
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- HackerNews/Controller/LogInViewController.swift
+++ HackerNews/Controller/LogInViewController.swift
@@ -7,24 +7,84 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
-class LogInViewController: UIViewController {
+class LogInViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
+    
 
+    
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+   
+    
+    @IBAction func LogInButtonPressed(_ sender: UIButton) {
+        
+        if (emailTextField.text?.isEmpty == false && passwordTextField.text?.isEmpty == false) {
+            
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (result, error) in
+                
+                if error != nil {
+                    print(error!)
+                } else {
+                    print("Registration Successful")
+                }
+                
+            }
+            
+        }
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        GIDSignIn.sharedInstance()?.uiDelegate = self
+        GIDSignIn.sharedInstance()?.delegate = self
+        GIDSignIn.sharedInstance()?.signIn()
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK:-  Delegate Method of GoogleSignIn
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+        if let error = error {
+            print(error)
+            return
+        }
+        
+        guard let authentication = user.authentication else { return }
+        
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+        print(credential)
+        
+        Auth.auth().signInAndRetrieveData(with: credential) { (result, error) in
+            if error != nil {
+                print(error!)
+            } else {
+                print("Registration Successful")
+            }
+        }
+        
+        
     }
-    */
+    
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+        do {
+            try Auth.auth().signOut()
+        }
+        catch {
+            print("An error occurred while signing out")
+        }
+    }
+
 
 }
